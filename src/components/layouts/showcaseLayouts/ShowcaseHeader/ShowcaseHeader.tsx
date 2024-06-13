ca1d799fc7f71a07c805a20c0ff80ccefaa81d32
mtkk077 <154675808+mtkk077@users.noreply.github.com>
2024-06-13 17:58:08 +0600

    first commit

--- src/components/layouts/showcaseLayouts/ShowcaseHeader/ShowcaseHeader.tsx
+++ src/components/layouts/showcaseLayouts/ShowcaseHeader/ShowcaseHeader.tsx
@@ -24,7 +24,7 @@ const ShowcaseHeader: React.FC<IShowcaseHeaderProps> = () => {
         </Link>
 
         <a
-          href={'https://github.com/rizametovd/e-commerce-react-app'}
+          href={''}
           className={classes.link}
           target="_blank"
           rel="noreferrer"
